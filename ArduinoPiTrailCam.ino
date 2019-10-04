4abdae51e536a0fb1d7ea2d1cb6b8e41b2660721
d15c0 <essaiche@gmail.com>
2019-10-04 13:04:47 +1000

    Update ArduinoPiTrailCam.ino

--- ArduinoPiTrailCam.ino
+++ ArduinoPiTrailCam.ino
@@ -6,32 +6,30 @@ const byte DETECT = 3; // Motion Detect
 const byte PWR = 10; // PWR 
 const byte TRIG = 9; // camera trigger to pi GPIO 21
 const byte SHTDWN = 8; // shutdown pulse to pi GPIO 26
-const byte DETECTOUT = 7;
-
+const byte DETECTOUT = 13;
 
 unsigned long motionActivatedMillis; // when button was pushed
-unsigned long trigOffMillis;
+//unsigned long trigOffMillis;
 unsigned long pwrTurnedOnAt; // when pwr was turned on
 unsigned long trigTurnedOnAt; // blah trig not pwr
 
 unsigned long pwrOnDelay = 250; // wait to turn on PWR
-unsigned long pwrOffDelay = 180000; // turn off PWR after this time
+//unsigned long pwrOffDelay = 180000; // turn off PWR after this time
 unsigned long trigOnDelay = 250; // after motion wait to activate trigger
 unsigned long trigOffDelay = 20000; // keep the trigger low for ...
-unsigned long shtdwnOnDelay = 30000;
-unsigned long shtdwnOffDelay = 8000;
+unsigned long shtdwnOnDelay = 20000;
+//unsigned long shtdwnOffDelay = 8000;
 
 bool pwrReady = false; // flag for when button is let go
 bool pwrState = false; // for PWR is on or not.
 bool trigReady = false;
 bool trigState = false;
-bool shtdwnReady = false;
-bool shtdwnState = false;
+//bool shtdwnReady = false;
+//bool shtdwnState = false;
+bool needsShtdwnPulse = true;
 
 
-
-
-void setup() {
+void setup() { 
  pinMode(MOTION, INPUT);
  pinMode(DETECT, INPUT);
  pinMode(PWR, OUTPUT);
@@ -44,7 +42,9 @@ void setup() {
  digitalWrite(SHTDWN, HIGH); 
 
  pinMode(DETECTOUT, OUTPUT);
- digitalWrite(DETECTOUT, HIGH);
+ digitalWrite(DETECTOUT, LOW);   
+ 
+ Serial.begin(115200);
 }
 
 
@@ -55,7 +55,7 @@ void loop() {
 
  // check the button
  if (digitalRead(MOTION) == HIGH) {
-
+  Serial.println("in motion == high");
   // update the time when button was pushed
   motionActivatedMillis = currentMillis;
   pwrReady = true;
@@ -63,11 +63,12 @@ void loop() {
 
  }
 //check motion 
- if (digitalRead(DETECT) == HIGH) {
-    digitalWrite(DETECTOUT, LOW);
+ if (digitalRead(MOTION) == HIGH) {
+    Serial.println("in detect == high");
+    digitalWrite(DETECTOUT, HIGH);
  }
  else{
-  digitalWrite(DETECTOUT, HIGH);
+  digitalWrite(DETECTOUT, LOW);
  }
 
   
@@ -75,15 +76,23 @@ void loop() {
  // make sure this code isn't checked until after button has been let go
 
  if (pwrReady) {
-  
+   Serial.println("in pwrReady");
    //this is typical millis code here:
    if ((currentMillis - motionActivatedMillis) <= pwrOnDelay) { 
-
+     Serial.println("in pwrReady if");
      delay(1500);
   
      // okay, enough time has passed since the button was let go.
      digitalWrite(PWR, HIGH);
 
+     // pulse the SHTDWN to high to turn the pi on
+     if (needsShtdwnPulse) {
+        digitalWrite(SHTDWN, LOW);
+        delay(2000);
+        digitalWrite(SHTDWN, HIGH); 
+        needsShtdwnPulse = false; 
+     }
+     
      // setup our next "state"
      pwrState = true;
 
@@ -97,10 +106,10 @@ void loop() {
  }
 
  if (trigReady) {
-
+   Serial.println("in trigready");
    //this is typical millis code here:
    if (currentMillis - motionActivatedMillis <= trigOnDelay) {  
-
+     Serial.println("in trigReady 1st if");
      delay(3000);
    
      // okay, enough time has passed since the button was let go.
@@ -114,34 +123,33 @@ void loop() {
 
      // wait for next button press
      trigReady = false;
-
    }
  }
 
  
 // see if we are watching for the time to turn off TRIG
  if (trigState) {
-
+   Serial.println("in trigState");
    // okay, pwr on, check for now long
    if ((currentMillis - trigTurnedOnAt) >= trigOffDelay) {
-
+     Serial.println("in trigState if");
      trigState = false;
-     digitalWrite(TRIG, HIGH);
-                    
-    
+     digitalWrite(TRIG, HIGH);               
    }
 
 }
 
 if (pwrState) {
-
+  Serial.println("in pwrState");
   if ((currentMillis - trigTurnedOnAt) >= (trigOffDelay + shtdwnOnDelay)) {
+    Serial.println("in pwrState if");
     pwrState = false;
     trigState = false;
     digitalWrite(SHTDWN, LOW);
+    needsShtdwnPulse = true;
     delay(2000);
     digitalWrite(SHTDWN, HIGH);
-    delay(6000);
+    delay(30000);
     digitalWrite(PWR, LOW);
    
    }
